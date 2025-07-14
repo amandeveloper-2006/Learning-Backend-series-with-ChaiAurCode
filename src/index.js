0c39aa6c723da55a762c47fa6e01d29da993d414
Aman Raj <araj2271770@gmail.com>
2025-07-14 19:10:03 +0530

    api handling

--- src/index.js
+++ src/index.js
@@ -1,49 +1,51 @@
-//require("dotenv").config();  //code consitency is bad practice
-
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants.js";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
 
- dotenv.config({
-    path: "./.env"
- });
+
 
 connectDB()
-.then(()=>{
+.then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
-.catch((error) => {
-    console.error(` MongoDB connection Failed ${error.message}`);
-    process.exit(1); // Exit the process with failure
-});
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
 
 
 
 /*
-import express from "express";
-const app = express();
-
-// iife funcation is alway start with semicolon only for cleaning up the code 
-
-;(async ()=>{
+import express from "express"
+const app = express()
+( async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-        app.on("error", (err) => {
-            console.error(`Server error: ${err.message}`);
-        });
-app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        }
-        )
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
     } catch (error) {
-        console.error(`Error: ${error.message}`);
-        throw error;
-        
+        console.error("ERROR: ", error)
+        throw err
     }
 })()
+
 */
-
-
